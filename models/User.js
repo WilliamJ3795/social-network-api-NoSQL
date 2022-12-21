f2e60106eaedd8c3b35cb15ed7f36ffd927f708a
William James <Williamj3795@gmail.com>
2022-12-20 21:46:15 -0500

    viritual added

--- models/User.js
+++ models/User.js
@@ -2,3 +2,45 @@
 const moment = require('moment')
 const { Schema, model } = require('mongoose')
 
+const UserSchema = new Schema(
+    {
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        // Regex (regular expression) used to find all characters for an email address
+        match: [
+          /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+          'Enter a valid email address',
+        ],
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Thought',
+        },
+      ],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      ],
+    },
+    {
+      //we want virtuals to be included with our response
+  
+      toJSON: {
+        virtuals: true,
+        getters: true,
+      },
+      id: false,
+    },
+  )
