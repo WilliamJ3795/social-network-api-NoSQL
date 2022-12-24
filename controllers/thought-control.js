2e15ec4fff6ec302d5c860ac6db7ef0dd28fd24b
William James <Williamj3795@gmail.com>
2022-12-24 16:31:50 -0500

    thought control function partially finished:

--- controllers/thought-control.js
+++ controllers/thought-control.js
@@ -1,1 +1,30 @@
 const { Thought, User } = require('../models')
+
+const thoughtControl = { 
+    getAllThoughts(req, res) {
+      Thought.find({})
+        .select('-__v')
+        .then((dbThoughtData) => res.json(dbThoughtData))
+        .catch((err) => {
+          console.log(err)
+          res.status(500).json(err)
+        })
+    },
+    createThought({ params, body }, res) {
+        Thought.create(body)
+          .then(({ _id }) => {
+            return User.findOneAndUpdate(
+              { _id: body.userId },
+              { $push: { thoughts: _id } },
+              { new: true },
+            )
+          })
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No user found with this username!' })
+              return
+            }
+            res.json(dbUserData)
+          })
+          .catch((err) => res.json(err))
+      },
