ffb0bcdb772dcdf2f0a9f96d72099b3b872f83fd
William James <Williamj3795@gmail.com>
2022-12-25 11:06:40 -0500

    delete User route

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -53,3 +53,15 @@ const userController = {
       })
       .catch((err) => res.json(err))
   },
+  
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'User with that ID not found!!' })
+          return
+        }
+        res.json(dbUserData)
+      })
+      .catch((err) => res.status(400).json(err))
+  },
