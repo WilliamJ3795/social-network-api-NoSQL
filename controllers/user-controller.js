d1f2fdc2d5fb57dea4bbf91377869f46fb4f9cab
William James <Williamj3795@gmail.com>
2022-12-24 19:45:54 -0500

    getUserByID route

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -14,3 +14,21 @@ const userController = {
         res.status(500).json(err)
       })
   },
+  //getting Users only by Id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: 'thoughts',
+        select: '-__v',
+      })
+      .populate({
+        path: 'friends',
+        select: '-__v',
+      })
+      .select('-__v')
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err)
+        res.status(500).json(err)
+      })
+  },
