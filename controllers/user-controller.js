3ccae9690a77a7f5faf1ea6d122a8a3a599bb594
William James <Williamj3795@gmail.com>
2022-12-24 19:51:06 -0500

    update user route created

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -32,8 +32,24 @@ const userController = {
         res.status(500).json(err)
       })
   },
+
   createUser({ body }, res) {
     User.create(body)
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(400).json(err))
+  },
+
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'User with that ID could not be found' })
+          return
+        }
+        res.json(dbUserData)
+      })
+      .catch((err) => res.json(err))
   },
